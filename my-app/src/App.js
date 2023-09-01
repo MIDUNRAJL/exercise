ec5dff908d259cd89ff3096fd55bb6e605a0b66c
MIDUNRAJ <miduntvm15@gmail.com>
2023-09-01 21:59:45 +0530

    cpompleted

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,38 +1,30 @@
 import React, { useState } from 'react';
 import './App.css';
+import Button from './Button'
 
 function App() {
-  const [formData, setFormData] = useState({muscleGroup: ""})
-  const [savedId, setSavedId] = useState(null)
+  const [formData, setFormData] = useState({muscleGroup: "", level: ""});
+  // const [savedId, setSavedId] = useState(null)
   const [data, setData] = useState(null)
 
   const handleChange = (event) =>{
-    const{name, value, type} = event.target
+    const{name, value} = event.target
 
     setFormData((prevData) =>({
-      ...prevData, [name]: inputValue
+      ...prevData, [name]: value
     }))
   }
-  const handleSubmit = async (event) => {
+  const handleSubmit =  (event) => {
     event.preventDefault();
-    savedId(formData.muscleGroup)
-    var muscle = formData.muscleGroup
-$.ajax({
-    method: 'GET',
-    url: 'https://api.api-ninjas.com/v1/exercises?muscle=' + muscle,
-    headers: { 'X-Api-Key': 'i883KF+Ub1y46gZ7q9zgAA==ANzqtUvdOBx3MqjD'},
-    contentType: 'application/json',
-    success: function(result) {
-        console.log(result);
-    },
-    error: function ajaxError(jqXHR) {
-        console.error('Error: ', jqXHR.responseText);
-    }
-});
+    const {muscleGroup} = formData;
+    // var muscle = formData.muscleGroup
+    fetch(`https://api.api-ninjas.com/v1/exercises?muscle=${muscleGroup}`, {
+      headers: { 'X-Api-Key': 'i883KF+Ub1y46gZ7q9zgAA==ANzqtUvdOBx3MqjD' },
+    })
 
 .then(response => response.json())
 .then(result => {
-  console.log(result);
+  console.log(result)
   setData(result)
 })
 .catch(error => {
@@ -40,16 +32,20 @@ $.ajax({
 });
   }
 
+  const handleClick = (exerciseType) =>{
+    console.log(`Button clicked: ${exerciseType}`)
+  }
+
   
   return (
     <div className='exercise'>
         <h1>Exercise Guide</h1>
-       <div>
+    
 
         <h3>SELECT EXERCISE TYPE</h3>
 
        
-       <Button onClick
+       {/* <Button onClick={handleClick}
        className="custom-button">
         CARDIO
        </Button>
@@ -77,13 +73,13 @@ $.ajax({
        className="custom-button">
         STRONGMAN       
        </Button> 
-       </div>
+       */}
 
-       <div>
+       
         <form onSubmit={handleSubmit}>
         <label> SELECT THE MUSCLE GROUP </label> 
           <select
-                  name="types"
+                  name="muscleGroup"
                   value={formData.muscleGroup}
                   onChange={handleChange}>
                     <option value="">SELECT THE MUSCLE GROUP</option>
@@ -105,19 +101,21 @@ $.ajax({
                     <option value="triceps">Triceps</option>
                   
           </select>
-          </form>
+          
                   
-       </div>
-       <div>
+       
+       
 
-       <form onSubmit={handleSubmit}>
+       
 
       
 
       
         <label> SELECT THE DIFFICULTY LEVEL </label>
           <select
-                  name="level">
+                  name="level"
+                  value={formData.level}
+                  onChange={handleChange}>
            <option value="">SELECT THE DIFFICULTY LEVEL</option>  
            <option value="begin">Beginner</option>
            <option value="intermediate">Intermediate</option>
@@ -125,8 +123,43 @@ $.ajax({
                   </select>
                   <button type="submit">SUBMIT</button>
                   </form>
-                
-                  </div>
+                  {data && data.exercises && (
+                    <div>
+                      <h2>Fetched Exercises:</h2>
+                      <ul>
+                        {data.exercises.map((exercise, index) => (
+                          <li key={index}>{exercise.name}</li>
+                        ))}
+                      </ul>
+                      </div>
+                  )}
+
+<h3>SELECT EXERCISE TYPE</h3>
+                            <div>
+                              <button onClick={() => handleClick("Cardio")} className="custom-button">
+                                CARDIO
+                              </button>
+                              <button onClick={() => handleClick("Olympic Weightlifting")} className='custom-button'>
+                                OLYMPIC WEIGHTLIFTING
+                              </button>
+                              <button onClick={() => handleClick("plyometrics")} className='custom-button'>
+                                PLYOMETRICS
+                              </button>
+                              <button onClick={() => handleClick("powerlifting")} className='custom-button'>
+                                POWER LIFTING
+                              </button>
+                              <button onClick={() => handleClick("strength")} className='custom-button'>
+                                STRENGTH
+                              </button>
+                              <button onClick={() => handleClick("stretching")} className='custom-button'>
+                                STRETCHING
+                              </button>
+                              <button onClick={() => handleClick("strongman")} className='custom-button'>
+                                STRONGMAN
+                              </button>
+                            </div>
+
+                </div>
                    
 
   
